77b1be33a3508f0debea0e2f3bde159fe6c6bbff
roguelxx <xuanxin0213@gmail.com>
2019-03-30 20:00:45 +0800

    update 第五章 数组矩阵广义表

--- 天勤/例题/05数组矩阵广义表/习题真题.cpp
+++ 天勤/例题/05数组矩阵广义表/习题真题.cpp
@@ -177,8 +177,164 @@ void findMaxMatrix(int A[][4], int m, int n) {
         if (flag) {
             cout << maxi << ",[" << i << ", " << maxj << "]" << " ";
         }
+        cout << endl;
     }
-    cout << endl;
+}
+
+/*
+ * 简要介绍稀疏矩阵的三元组存储结构特点,并实现稀疏矩阵的基本操作.
+ * 1.给定稀疏矩阵A(int型),创建其三元组存储结构B
+ * 2.查找给定元素x是否在矩阵中
+ */
+
+void createTrimat(int A[][4], int m, int n, int B[][3]) {
+    int i, j, k = 1;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < n; ++j) {
+            if (A[i][j] != 0) {
+                B[k][0] = A[i][j];
+                B[k][1] = i;
+                B[k][2] = j;
+                ++k;
+            }
+        }
+    }
+    B[0][0] = k - 1;
+    B[0][1] = m;
+    B[0][2] = n;
+}
+
+int search(int B[][3], int x) {
+    int k = B[0][0];
+    int i;
+    for (i = 1; i <= k; ++i) {
+        if (B[i][0] == x) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/*
+ * 假设稀疏矩阵A采用三元组表示,编写一个函数,计算其转置矩阵B,
+ * 要求B也采用三元组表示.
+ */
+void reverseTrimat(int A[][3], int B[][3]) {
+    int p, q, col;
+    B[0][0] = A[0][0];
+    B[0][1] = A[0][1];
+    B[0][2] = A[0][2];
+    if (B[0][0] > 0) {
+        q = 1;
+        for (col = 0; col < B[0][1]; ++col) {
+            for (p = 1; p <= B[0][0]; ++p) {
+                if (A[p][2] == col) {
+                    B[q][0] = A[p][0];
+                    B[q][1] = A[p][2];
+                    B[q][2] = A[p][1];
+                    ++q;
+                }
+            }
+        }
+    }
+}
+
+/*
+ * 假设稀疏矩阵A和B(两矩阵行列数对应相等)都采用三元组表示,
+ * 编写一个函数,计算C=A+B,要求C也采用三元组表示,所有矩阵均为int型
+ */
+void addTrimat(int A[][3], int B[][3], int C[][3]) {
+    int i = 1, j = 1, k = 1;
+    while (i <= A[0][0] && j <= B[0][0]) {
+        if (A[i][1] == B[j][1]) {
+            if (A[i][2] < B[j][2]) {
+                C[k][0] = A[i][0];
+                C[k][1] = A[i][1];
+                C[k][2] = A[i][2];
+                ++k;
+                ++i;
+            } else if (A[i][2] < B[j][2]) {
+                C[k][0] = B[j][0];
+                C[k][1] = B[j][1];
+                C[k][2] = B[j][2];
+                ++k;
+                ++j;
+            } else if (A[i][2] == B[i][2]) {
+                C[k][0] = A[i][0] + B[j][0];
+                C[k][1] = B[j][1];
+                C[k][2] = B[j][2];
+                ++k;
+                ++i;
+                ++j;
+            }
+        } else if (A[i][1] < B[j][1]) {
+            C[k][0] = A[i][0];
+            C[k][1] = A[i][1];
+            C[k][2] = A[i][2];
+            ++k;
+            ++i;
+        } else if (A[i][1] > B[j][1]) {
+            C[k][0] = B[j][0];
+            C[k][1] = B[j][1];
+            C[k][2] = B[j][2];
+            ++k;
+            ++j;
+        }
+    }
+    while (i <= A[0][0]) {
+        C[k][0] = A[i][0];
+        C[k][1] = A[i][1];
+        C[k][2] = A[i][2];
+        ++k;
+        ++i;
+    }
+    while (j <= B[0][0]) {
+        C[k][0] = B[j][0];
+        C[k][1] = B[j][1];
+        C[k][2] = B[j][2];
+        ++k;
+        ++j;
+    }
+    C[0][0] = k - 1;
+    C[0][1] = A[0][1] > B[0][1] ? A[0][1] : B[0][1];
+    C[0][2] = A[0][2] > B[0][2] ? A[0][2] : B[0][2];
+}
+
+/*
+ * 假设稀疏矩阵A和B(分别为m×n和n×k矩阵)采用三元组表示,
+ * 表写一个函数,计算C=A×B,要求C也采用三元组表示的稀疏矩阵.
+ */
+int getValue(int D[][3], int i, int j) {
+    int k = 1;
+    while (k <= D[0][0] && D[k][1] == i && D[k][2] == j) {
+        ++k;
+    }
+    if (k <= D[0][0]) {
+        return D[k][0];
+    } else {
+        return 0;
+    }
+}
+
+void multipleTrimat(int A[][3], int B[][3], int C[][3], int m, int n, int k) {
+    int i, j, l, p = 1, s;
+    for (i = 0; i < m; ++i) {
+        for (j = 0; j < k; ++j) {
+            s=0;
+            for (l = 0; l < n; ++l) {
+                s += getValue(A, i, l) + getValue(B, l, j);
+            }
+            if (s != 0) {
+                C[p][0] = s;
+                C[p][1] = i;
+                C[p][2] = j;
+                ++p;
+            }
+        }
+    }
+    C[0][0] = p - 1;
+    C[0][1] = m;
+    C[0][2] = k;
 }
 
 void testFindMaxMatrix() {
