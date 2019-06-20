69e3c47c7ec861933705240bf7588313fd5de78d
roguelxx <xuanxin0213@gmail.com>
2019-06-20 18:08:54 +0800

    update 第四章 树与二叉树

--- 王道/04 树与二叉树/4.3 test.cpp
+++ 王道/04 树与二叉树/4.3 test.cpp
@@ -264,3 +264,193 @@ void DelByElemType2(BiTree T, ElemType x) {
         }
     }
 }
+
+/*
+ * 设一棵二叉树的结点结构为(llink,info,rlink),root为指向该二叉树根节点的指针,
+ * p和q分别为指向该二叉树中任意两个结点的指针,编写算法ancestor(root, p, q, r),
+ * 找到p和q的最近公共祖先r,假设p在q的左边.
+ */
+typedef struct {
+    BiTree t;
+    int tag;    // tag=0表示左子女已被访问,tag=1表示右子女已被访问
+} stack;
+stack s[], s1[];
+BiTree Ancestor(BiTree root, BiTNode *p, BiTNode *q) {
+    int top = -1, i, j;
+    BiTree bt = root;
+    while (bt != NULL || top > -1) {
+        while (bt != NULL && bt != p && bt != q) {
+            s[++top] = bt;
+            s[top].tag = 0;
+            bt = bt->llink;
+        }
+        while (top > -1 && s[top].tag == 1) {
+            if (s[top].t == p) {
+                for (i = 0; i <= top; ++i) {
+                    s1[i] = s[i];
+                    top1 = top;
+                }
+            }
+            if (s[top].t == q) {
+                for (i = top; i > -1; --i) {
+                    for (j = top1; j > -1; --j) {
+                        if (s1[j].t == s[i].t) {
+                            return s[i].t;
+                        }
+                    }
+                }
+                --top;
+            }
+        }
+        if (top != -1) {
+            s[top].tag = 1;
+            bt = s[top].t->rchild;
+        }
+    }
+    return NULL;
+}
+
+/*
+ * 假设二叉树采用二叉链表存储结构,设计一个算法,求非空二叉树b的宽度.
+ */
+typedef struct {
+    BiTree data[maxSize];
+    int level[maxSize];
+    int front, rear;
+} q;
+
+int BiTreeWidth(BiTree b) {
+    BiTree p;
+    int max, i, n, k;
+    q.rear = q.front = -1;
+    q.rear++;
+    q.data[q.rear] = b;
+    q.level[q.rear] = 1;
+    while (q.rear > q.front) {
+        q.front++;
+        p = q.data[q.front];
+        k = q.level[q.front];
+        if (p->lchild) {
+            q.rear++;
+            q.data[q.rear] = p->lchild;
+            q.level[q.rear] = k + 1;
+        }
+        if (p->rchild) {
+            q.rear++;
+            q.data[q.rear] = p->rchild;
+            q.level[q.rear] = k + 1;
+        }
+    }
+    max = 0, i = 0, k = 1;
+    while (i <= q.rear) {
+        n = 0;
+        while (i<=q.rear&&q.level[i] == k) {
+            n++;
+            i++;
+        }
+        if (n > max) {
+            max = n;
+        }
+    }
+    return max;
+}
+
+/*
+ * 设有一棵满二叉树(所有结点值均不同),已知其先序序列为pre,设计一个算法求其后序序列post.
+ */
+void PreToPost(ElemType pre[], int l1, int h1, ElemType post[], int l2, int h2) {
+    int half;
+    if (l1 <= h1) {
+        half = (h1 - l1) / 2;
+        post[h2] = pre[l1];
+        PreToPost(pre, l1 + 1, l1 + half, post, l2, l2 + half - 1);
+        PreToPost(pre, l1 + half + 1, h1, post, l2 + half, h2 - 1);
+    }
+}
+
+/*
+ * 设计一个算法将二叉树的结点按从左到右的顺序连成一个单链表,表头指针为head.二叉树
+ * 按照二叉链表方式存储,连接时用叶结点的右指针域来存放单链表指针.
+ */
+LinkList LinkLeafNodes(BiTree T, LinkList *head) {
+    LinkList *pre = NULL;
+    BiTree p = T;
+    InitStack(S);
+    while (p != NULL||!Empty(S)) {
+        if (p) {
+            Push(S, p);
+            p = p->lchild;
+        } else {
+            Pop(S, p);
+            if (p->lchild == NULL && p->rchild == NULL) {
+                if (head == NULL) {
+                    head = p;
+                    pre = p;
+                } else {
+                    pre->rchild = p;
+                    pre = p;
+                }
+            }
+            p = p->rchild;
+        }
+    }
+    pre->rchild = NULL;
+    return head;
+}
+
+LinkedList head, pre = NULL;
+LinkList LinkLeafNodes2(BiTree T) {
+    if (T != NULL) {
+        LinkLeafNodes2(T->lchild);
+        if (T->lchild == NULL && T->rchild == NULL) {
+            if (head == NULL) {
+                head = T;
+                pre = T;
+            } else {
+                pre->rchild = T;
+                pre = T;
+            }
+        }
+        LinkLeafNodes2(T->rchild);
+        pre->rchild = NULL;
+    }
+    return head;
+}
+
+/*
+ * 设计一个算法,判断两棵二叉树是否相似.所谓相似,指的是T1和T2都是空的二叉树或都
+ * 只有一个根结点;或T1的左子树和T2的左子树相似,T1的右子树和T2的右子树相似.
+ */
+bool IsSimilar(BiTree T1, BiTree T2) {
+    if (T1 == NULL && T2 == NULL) {
+        return true;
+    } else if (T1 == NULL || T2 == NULL) {
+        return false;
+    } else {
+        return IsSimilar(T1->lchild, T2->lchild) && IsSimilar(T1->rchild, T2->rchild);
+    }
+}
+
+/*
+ * 写出在中序线索二叉树中查找指定结点在后序的前驱结点的算法.
+ */
+BiThrTree InPostPre(BiThrTree t, BiThrTree p) {
+    BiThrTree q;
+    if (p->rtag == 0) {
+        q = p->rchild;
+    } else if (p->ltag == 0) {
+        q = p->lchild;
+    } else if (p->lchild == NULL) {
+        q = NULL;
+    } else {
+        while (p->ltag == 1 && p->lchild != NULL) {
+            p = p->lchild;
+        }
+        if (p->ltag == 0) {
+            q = p->lchild;
+        } else {
+            q = NULL;
+        }
+    }
+    return q;
+}
